7ad973de9259e48b3e08b4d743aafa5d086e142a
ShakeforProtein <github@taktimer.net>
2020-08-11 14:38:55 +1000

    Removed lore based enchantment system. Updated to 1.16 Disables gamepedia wiki linking to avoid interfering with sickle.

--- src/main/java/me/shakeforprotein/shakenbt/Listeners/onHoe.java
+++ src/main/java/me/shakeforprotein/shakenbt/Listeners/onHoe.java
@@ -12,42 +12,44 @@ import org.bukkit.inventory.EquipmentSlot;
 public class onHoe implements Listener {
 
     @EventHandler
-    public void onPlayerHoe(PlayerInteractEvent e){
-        if(e.getAction() == Action.LEFT_CLICK_BLOCK && e.getItem() != null && e.getItem().getType() == Material.WOODEN_HOE){
-            e.setCancelled(true);
-            String type = e.getClickedBlock().getType().name().toLowerCase();
-            String[] typeList = type.split("_");
-            String newNewType = "";
-            for(String newType : typeList){
-                newType = newType.substring(0, 1).toUpperCase() + newType.substring(1);
-                if(newNewType.equals("")) {
-                    newNewType = newType;
+    public void onPlayerHoe(PlayerInteractEvent e) {
+        if (e.getPlayer().getLocation().getWorld().getName().toLowerCase().contains("hardcore")) {
+            if (e.getAction() == Action.LEFT_CLICK_BLOCK && e.getItem() != null && e.getItem().getType() == Material.WOODEN_HOE) {
+                e.setCancelled(true);
+                String type = e.getClickedBlock().getType().name().toLowerCase();
+                String[] typeList = type.split("_");
+                String newNewType = "";
+                for (String newType : typeList) {
+                    newType = newType.substring(0, 1).toUpperCase() + newType.substring(1);
+                    if (newNewType.equals("")) {
+                        newNewType = newType;
+                    } else {
+                        newNewType = newNewType + "_" + newType;
+                    }
                 }
-                else{
-                    newNewType = newNewType + "_" + newType;
-                }
+                e.getPlayer().sendMessage("https://minecraft.gamepedia.com/" + newNewType);
             }
-            e.getPlayer().sendMessage("https://minecraft.gamepedia.com/" + newNewType);
         }
     }
 
     @EventHandler
-    public void onPlayerHoe(PlayerInteractAtEntityEvent e){
-        if(e.getPlayer().getInventory().getItemInMainHand().getType() == Material.WOODEN_HOE && e.getHand() == EquipmentSlot.HAND){
-            e.setCancelled(true);
-            String type = e.getRightClicked().getType().name().toLowerCase();
-            String[] typeList = type.split("_");
-            String newNewType = "";
-            for(String newType : typeList){
-                newType = newType.substring(0, 1).toUpperCase() + newType.substring(1);
-                if(newNewType.equals("")) {
-                    newNewType = newType;
+    public void onPlayerHoe(PlayerInteractAtEntityEvent e) {
+        if (e.getPlayer().getLocation().getWorld().getName().toLowerCase().contains("hardcore")) {
+            if (e.getPlayer().getInventory().getItemInMainHand().getType() == Material.WOODEN_HOE && e.getHand() == EquipmentSlot.HAND) {
+                e.setCancelled(true);
+                String type = e.getRightClicked().getType().name().toLowerCase();
+                String[] typeList = type.split("_");
+                String newNewType = "";
+                for (String newType : typeList) {
+                    newType = newType.substring(0, 1).toUpperCase() + newType.substring(1);
+                    if (newNewType.equals("")) {
+                        newNewType = newType;
+                    } else {
+                        newNewType = newNewType + "_" + newType;
+                    }
                 }
-                else{
-                    newNewType = newNewType + "_" + newType;
-                }
+                e.getPlayer().sendMessage("https://minecraft.gamepedia.com/" + newNewType);
             }
-            e.getPlayer().sendMessage("https://minecraft.gamepedia.com/" + newNewType);
         }
     }
 }
